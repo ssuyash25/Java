5228822c0e4280b7a06554a2e3d4bff30746cc08
suyashsrivastava25@gmail.com <suyashsrivastava25@gmail.com>
2021-06-06 22:29:15 +0530

    MongoDB using data-mongodb

---
+++ SpringBoot/mongoDemo/src/main/java/com/monoDemo/MongodbApplication.java
@@ -0,0 +1,10 @@
+package com.monoDemo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbApplication {
+    SpringApplication.run(MongodbApplication.class, args);
+}
